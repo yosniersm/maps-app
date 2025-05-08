978020dd2f8d2c395ff38ea1fd4eea28a032d22f
Yosnier Samon <yosamac@gmail.com>
2025-05-08 16:06:29 +0200

    chore: Add mapbox-gl module

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,17 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
+  <RouterView />
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
+    <!-- <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
-    </div>
+    </div> -->
   </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
@@ -24,8 +23,10 @@ header {
 }
 
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+  position: fixed;
+  bottom: 30px;
+  right: 30px;
+  width: 100px;
 }
 
 nav {
